2c8d577ba5a83683db411a4a42f80625ae3440b3
estebancasallasmila-blip <estebancasallasmila@gmail.com>
2026-02-22 19:36:05 -0500

    Add files via upload

--- Corte 1/Practica 1/HolaMundo.java
+++ Corte 1/Practica 1/HolaMundo.java
@@ -1,1 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package holamundo;
 
+/**
+ *
+ * @author esteb
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+          System.out.println("Hola mundo");
+        // TODO code application logic here
+    }
+    
+}
