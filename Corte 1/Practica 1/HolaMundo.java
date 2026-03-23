fcfdb83b508079273d0eebb9a097e8a87aa45bd6
estebancasallasmila-blip <estebancasallasmila@gmail.com>
2026-03-23 12:34:41 -0500

    Refactor HolaMundo.java with comments and formatting

--- Corte 1/Practica 1/HolaMundo.java
+++ Corte 1/Practica 1/HolaMundo.java
@@ -9,10 +9,11 @@ public class HolaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-          // 1. Cambiar el mensaje por tu nombre
+        //EJERCICIO DE HOLA MUNDO, guia prcatica
+          // 1. Aqui añadi mi nombre
         System.out.println("Hola, soy Esteban");
 
-        // 2. Imprimir dos líneas distintas
+        // 2. Imprimi dos líneas distintas
         System.out.println("Esta es la segunda línea del programa.");
         System.out.println("PROGRAMACION ORIENTADA A .");
 
