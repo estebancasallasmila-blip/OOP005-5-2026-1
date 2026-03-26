c9a9a9009a44661155da32e4e8eafa762f604bfa
estebancasallasmila-blip <estebancasallasmila@gmail.com>
2026-03-26 09:19:33 -0500

    Add files via upload

--- Corte 2/Prectica 1/Punto.java
+++ Corte 2/Prectica 1/Punto.java
@@ -1,1 +1,29 @@
 
+package Constructores;
+
+/**
+ *
+ * @author esteban
+ */
+
+public class Punto{
+    // Ejercicio 1: Atributos x y y
+    public int x;
+    public int y;
+
+    // Ejercicio 2: Constructor vacío (pone el punto en 0,0)
+    public Punto() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    // Ejercicio 2: Constructor con dos parámetros
+    public Punto(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void mostrar() {
+        System.out.println("Coordenadas: (" + x + ", " + y + ")");
+    }
+}
