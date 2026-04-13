dd938eab6bb6057ff53ab4f47d62f0fe8ca21f69
estebancasallasmila-blip <estebancasallasmila@gmail.com>
2026-04-13 13:26:49 -0500

    Add files via upload

--- Corte 2/Practica 3/Banck.java
+++ Corte 2/Practica 3/Banck.java
@@ -1,1 +1,20 @@
 
+package banck;
+public class Banck {
+    private Cuenta accountOne;
+    private Cuenta accountTwo;
+    
+    public Banck(){
+        accountOne =new Cuenta("Zeus",100,"1");
+        accountTwo =new Cuenta("Hades",200,"2");
+    }
+    
+    public static void main(String[] args) {
+      Banck banckOfGods=new Banck();
+      System.out.println(banckOfGods.accountOne.getBalance());
+      banckOfGods.accountOne.setBalance(5000);
+      System.out.println(banckOfGods.accountOne.getBalance());
+      System.out.println(banckOfGods.accountOne.getMonthlyInterenst());
+    }
+    
+}
